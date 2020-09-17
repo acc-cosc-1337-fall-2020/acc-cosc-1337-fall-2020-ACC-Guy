34518674603642a33ca629d44d7ef9a8f20591e9
ACC-Guy <adrian.fabian@g.austincc.edu>
2020-09-16 19:10:36 -0500

    while example

--- src/examples/04_module/01_while/main.cpp
+++ src/examples/04_module/01_while/main.cpp
@@ -1,7 +1,17 @@
+#include <iostream>
 #include "while.h"
+
+using std::cout; using std::cin;
 
 int main() 
 {
+	char choice = 'y';
+	
+	while(choice == 'y' || choice == 'Y')
+	{
+		cout<<"Enter y to continue, any other char to exit.";
+		cin>>choice;
+	}
 
 	return 0;
 }
--- src/examples/04_module/01_while/while.h
+++ src/examples/04_module/01_while/while.h
@@ -1,8 +1,9 @@
 #include<string>
 
 //Write function prototype named sum_of_squares that accepts an int and returns an int.
+int sum_of_squares(int sum);
 
-
+int sum_of_squares_2(int num);
 //write function prototype named display that accepts a string and returns void
 
 
