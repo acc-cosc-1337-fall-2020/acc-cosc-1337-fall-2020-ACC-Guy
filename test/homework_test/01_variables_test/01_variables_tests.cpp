c7c0339e15845a77da2b9bfdb80bc1cdd5ac015f
ACC-Guy <adrian.fabian@g.austincc.edu>
2020-09-18 22:14:57 -0500

    01 hw works and 4 tests passed

--- test/homework_test/01_variables_test/01_variables_tests.cpp
+++ test/homework_test/01_variables_test/01_variables_tests.cpp
@@ -12,3 +12,19 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE("01 Homework b")
+{
+	REQUIRE(get_sales_tax_amount(10) == .675);
+}
+TEST_CASE("01 Homework c")
+{
+	REQUIRE(get_sales_tax_amount(20) == 1.35);
+}
+TEST_CASE("01 Homework d")
+{
+	REQUIRE(get_tip_amount(20, .15) == 3);
+}
+TEST_CASE("01 Homework e")
+{
+	REQUIRE(get_tip_amount(20, .20) == 4);
+}
