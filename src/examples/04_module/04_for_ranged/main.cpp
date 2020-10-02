7f1484f50e63f2d3881c7cde82a8ee5bd2090150
ACC-Guy <adrian.fabian@g.austincc.edu>
2020-10-01 22:15:54 -0500

    examples in class update

--- src/examples/04_module/04_for_ranged/main.cpp
+++ src/examples/04_module/04_for_ranged/main.cpp
@@ -2,8 +2,42 @@
 #include<iostream>
 #include "for_ranged.h"
 
+using std::string; using std::cout;
+
 int main() 
 {
+	string name = "John Doe";
+	string name1();
+	string name2(name, 2, 4);
+	string name3("constructor copy");
+	string name4("I am too long for the buffer", 8);
+	string name5(10, 'x');
+
+	cout<<name<<"\n";
+	cout<<name1<<"\n";
+	cout<<name2<<"\n";
+	cout<<name3<<"\n";
+	cout<<name4<<"\n";
+	cout<<name5<<"\n";
+
+	cout<<name[2]<<"\n";
+	name[0] = 'j';
+	name[5] = 'd';
+	cout<<name<<"\n";
+
+	
+
+	for(std::size_t i=0; i < name.size(); ++i)
+	{
+		cout<<name[i]<<"\n";
+	}
+	cout<<"for ranged \n \n";
+	for(char c: name)
+	{
+		cout<<c<<"\n";
+	}
+
+
 
 	return 0;
 }
--- src/examples/04_module/06_vectors/main.cpp
+++ src/examples/04_module/06_vectors/main.cpp
@@ -1,7 +1,31 @@
 #include "vec.h"
+#include<iostream>
+#include<vector>
+
+using std::cout; using std::vector;
 
 int main() 
 {
+	vector<int> nums{10, 3, 2};
+	
+	cout<<"Capacity: "<<nums.capacity()<<"\n";
+	nums.push_back(20);
+
+	cout<<"Capacity: "<<nums.capacity()<<"\n";
+
+	cout<<nums[3]<<"\n";
+
+	for(std::size_t i=0; i < nums.size(); ++i)
+	{
+		cout<<nums[i]<<"\n";
+	}
+
+	cout<<"\n for ranged \n";
+
+	for(auto n: nums)
+	{
+		cout<<n<<"\n";
+	}
 
 	return 0;
 }
